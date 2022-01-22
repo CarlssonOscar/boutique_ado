2de2659523d1e64812b4e83829803be78d54fd24
CarlssonOscar <oosccar@gmail.com>
2022-01-22 12:45:12 +0000

    Ongoing

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,11 +2,7 @@ from django.http import HttpResponse
 
 
 class StripeWH_Handler:
-    """
-    Handle Stripe webhooks
-    __init__ Constructors are used to initialize the object’s state. The task of constructors is to initialize(assign values)
-    to the data members of the class when an object of class is created.
-    """
+    """Handle Stripe webhooks"""
 
     def __init__(self, request):
         self.request = request
@@ -14,6 +10,22 @@ class StripeWH_Handler:
     def handle_event(self, event):
         """
         Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe
         """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
