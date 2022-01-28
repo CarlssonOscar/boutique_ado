f2ea6685be910007a12411323209a71a7397b553
CarlssonOscar <oosccar@gmail.com>
2022-01-28 08:55:00 +0000

    Removed secret key and set debug

--- ado_project/settings.py
+++ ado_project/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ixwzt&-sm*qxiao%_ueduu#2d0xn1!gh)^1(%qzzr+2@ur^v4o'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ado-store.herokuapp.com', 'localhost']
 
